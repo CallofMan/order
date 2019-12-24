cbc068c38379870adad91d93c3120bda5c340bea
ilya5006 <samarcev.ilya@mail.ru>
2019-12-24 17:33:41 +0300

    User info in admin panel

--- adminpanel.php
+++ adminpanel.php
@@ -1,6 +1,9 @@
 <?php
 require_once "connection.php";
 session_start();
+
+$idUser = $_GET['id_user'];
+
 if ($_SESSION['id_user'] != 1)
 {
     header("Location: index.php");
@@ -10,6 +13,7 @@ if (isset($_GET['deleteUser']))
 {
     $userId = $_GET['idUser'];
     mysqli_query($link, "DELETE FROM users WHERE id_user = '$userId'");
+    header("Location: adminpanel.php");
 }
 
 if(isset($_GET['addUser']))
@@ -37,24 +41,55 @@ if(isset($_GET['addUser']))
             while ($user = mysqli_fetch_assoc($usersQuery))
             {
                 echo "
-                <div class='user'>
-                    <p class='listUserBlock'>".$user['first_name']." ".$user['second_name']." ".$user['position']."</p>
-                </div>";
+                <form method='GET' action='' class='user'>
+                    <input type='text' name='id_user' value='".$user['id_user']."' style='display: none;'>
+                    <button class='listUserBlock'>".$user['first_name']." ".$user['second_name']." ".$user['position']."</button>
+                </form>";
             }
             
             ?>
         </div>
 
         <form id="infoUser" action="" method="GET">
-            <p>ntcnпра</p>
-            <p>dfgdfg</p>
-            <p>sdgfgdsfg</p>
-            <p>sdfgsdfgdsfg</p>
-            <p>sdfgsdfg</p>
-            <p>gfdghfth</p>
-            <p>fghfghhfgdd</p>
+            <?php
+            if(isset($idUser))
+            {
+            $userInfo = mysqli_query($link, "SELECT * FROM users WHERE id_user = '$idUser'");
+            $user = mysqli_fetch_assoc($userInfo);
+
+            $userId = $user['id_user'];
+            $firstName = $user['first_name'];
+            $secondName = $user['second_name'];
+            $login = $user['login'];
+            $password = $user['password'];
+            $role = $user['role'];
+            $position = $user['position'];
+            $email = $user['email'];
+            $telephone = $user['telephone'];
+            $address = $user['address'];
+
+            $role = mysqli_query($link, "SELECT name_role FROM roles WHERE role = '$role'");
+            $role = mysqli_fetch_row($role);
+            $role = $role[0];
+
+            echo "
+            <p>ID User -$userId</p>
+            <p>Имя - $firstName</p>
+            <p>Фамилия - $secondName</p>
+            <p>Логин - $login</p>
+            <p>Паоль - $password</p>
+            <p>Роль - $role</p>
+            <p>Должность - $position</p>
+            <p>Email - $email</p>
+            <p>Телефон - $telephone</p>
+            <p>Адрес - $address</p>
+            ";
+            ?>
+            <input type="text" name="idUser" value="<?php echo $userId; ?>" style='display: none;'>
             <input type="submit" id='deleteUser' name='deleteUser' value='Удалить пользователя'>
             <input type="submit" id='addUser' name='addUser' value='Добавить нового пользователя'>
+      <?php 
+            } ?>
         </form>
     </div>
     
